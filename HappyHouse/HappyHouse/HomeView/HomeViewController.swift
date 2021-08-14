1d90edef1049bf5b5fa2de4b9c1a2404631fe5a3
leegeonwoo <lgw9898@naver.com>
2021-08-15 00:00:07 +0900

    rx적용 직전

--- HappyHouse/HappyHouse/HomeView/HomeViewController.swift
+++ HappyHouse/HappyHouse/HomeView/HomeViewController.swift
@@ -11,10 +11,24 @@ import RxCocoa
 
 class HomeViewController : UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var roomTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleUI()
     }
     
+}
+
+extension HomeViewController {
+    func titleUI() {
+        roomTitle.text = "서버에서 정보를 받아오자" //서버에서 룸 정보 받아오기;
+        roomTitle.backgroundColor = #colorLiteral(red: 1, green: 0.9017189145, blue: 0.9212634563, alpha: 1)
+        roomTitle.layer.cornerRadius = 12
+        roomTitle.clipsToBounds = true
+        roomTitle.font = UIFont(name: roomTitle.font.fontName, size: 11)
+    }
     
     
 }
