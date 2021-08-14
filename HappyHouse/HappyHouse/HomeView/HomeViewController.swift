9344a294d22b2400e996fefa25549b45f2e1dd17
leegeonwoo <lgw9898@naver.com>
2021-08-14 23:47:44 +0900

    홈뷰 레이아웃 잡기 완료

---
+++ HappyHouse/HappyHouse/HomeView/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  HappyHouse
+//
+//  Created by Hamlit Jason on 2021/08/14.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class HomeViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
