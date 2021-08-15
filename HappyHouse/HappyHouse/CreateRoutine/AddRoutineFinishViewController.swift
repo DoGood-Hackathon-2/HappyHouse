b4236675df317298bf32fe972b1d164aaf114972
Danna <songda515@gmail.com>
2021-08-15 13:00:09 +0900

    [Routine] 루틴 추가 완료 후 화면 이동

--- HappyHouse/HappyHouse/CreateRoutine/AddRoutineFinishViewController.swift
+++ HappyHouse/HappyHouse/CreateRoutine/AddRoutineFinishViewController.swift
@@ -18,15 +18,17 @@ class AddRoutineFinishViewController: UIViewController {
 
     @IBAction func showMypage(_ sender: Any) {
         let storyboard = UIStoryboard.init(name: "HomeView", bundle: nil)
-        let vc = storyboard.instantiateViewController(identifier: "MypageViewController")
+        let vc = storyboard.instantiateViewController(identifier: "HomeViewTabBar") as! HomeTabBarViewController
         vc.modalPresentationStyle = .fullScreen
+        vc.isHome = false
         present(vc, animated: true, completion: nil)
     }
     
     @IBAction func showHome(_ sender: Any) {
         let storyboard = UIStoryboard.init(name: "HomeView", bundle: nil)
-        let vc = storyboard.instantiateViewController(identifier: "HomeViewTabBar")
+        let vc = storyboard.instantiateViewController(identifier: "HomeViewTabBar") as! HomeTabBarViewController
         vc.modalPresentationStyle = .fullScreen
+        vc.isHome = true
         present(vc, animated: true, completion: nil)
     }
 }
