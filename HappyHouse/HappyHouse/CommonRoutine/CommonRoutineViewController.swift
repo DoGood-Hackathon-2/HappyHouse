c2f2af2326a2e0460db00b27640bfdd64ab1eebe
lgvv <lgw9898@naver.com>
2021-09-04 16:47:16 +0900

    입력할 수 있는 년,월,일 제약조건 설정 완료 + UX 개선작업 완료

--- HappyHouse/HappyHouse/CommonRoutine/CommonRoutineViewController.swift
+++ HappyHouse/HappyHouse/CommonRoutine/CommonRoutineViewController.swift
@@ -67,7 +67,11 @@ class CommonRoutineViewController : UIViewController {
     var figmaHeight : CGFloat = 896 // 414 * 896
     var DeviceRatio : CGFloat = 1.0
     
-    let InnerBoxViewModel = WeekCollectionViewModel() // 매주, 월, 화 ...
+    let Yearborder = CALayer() // 텍스트 필드에 값을 잘못 입력할 경우 사용자에게 알려주기 인지시켜 주기 위해 underline
+    let Monthborder = CALayer()
+    let Dayborder = CALayer()
+    //let Yearborder = CALayer()
+    
     
     
     
@@ -308,23 +312,55 @@ extension CommonRoutineViewController {
         let figmaBoxRatio : CGFloat = 374 * 219
         let DateBoxRatio = DateBoxView.frame.width * 150 / figmaBoxRatio // 내 박스의 비율로 전환 - 세로길이 150 고정
         
+        let DateBoxWidth = view.frame.width - (view.frame.width/7)
+        let DateBoxWidthRatio = DateBoxWidth / 374
+        
+        
         YearTextField.then {
             $0.backgroundColor = UIColor(red: 0.913, green: 0.913, blue: 0.913, alpha: 1)
             $0.layer.cornerRadius = (DateBoxRatio * 17)
             $0.textAlignment = .center
             $0.borderStyle = .none
+            $0.keyboardType = .numberPad
+            $0.text = nowDateTime(0)
+            
+            // UnderLine
+            Yearborder.frame = CGRect(x: 0, y: $0.frame.size.height/4 * 3, width: 71.91 * DateBoxWidthRatio, height: 1.5)
+            Yearborder.cornerRadius =  (DateBoxRatio * 9)
+            Yearborder.backgroundColor = UIColor.green.cgColor
+            $0.layer.addSublayer(Yearborder)
+            Yearborder.isHidden = true
         }
         MonthTextField.then {
             $0.backgroundColor = UIColor(red: 0.913, green: 0.913, blue: 0.913, alpha: 1)
             $0.layer.cornerRadius = (DateBoxRatio * 17)
             $0.textAlignment = .center
             $0.borderStyle = .none
+            $0.keyboardType = .numberPad
+            $0.text = nowDateTime(1)
+            
+            // UnderLine
+            Monthborder.frame = CGRect(x: 0, y: $0.frame.size.height/4 * 3, width: 51.93 * DateBoxWidthRatio, height: 1.5)
+            Monthborder.cornerRadius =  (DateBoxRatio * 9)
+            Monthborder.backgroundColor = UIColor.red.cgColor
+            $0.layer.addSublayer(Monthborder)
+            Monthborder.isHidden = true
         }
         DayTextField.then {
             $0.backgroundColor = UIColor(red: 0.913, green: 0.913, blue: 0.913, alpha: 1)
             $0.layer.cornerRadius = (DateBoxRatio * 17)
             $0.textAlignment = .center
             $0.borderStyle = .none
+            $0.keyboardType = .numberPad
+            $0.text = nowDateTime(2)
+            
+            // UnderLine
+            Dayborder.frame = CGRect(x: 0, y: $0.frame.size.height/4 * 3, width: 51.93 * DateBoxWidthRatio, height: 1.5)
+            Dayborder.cornerRadius =  (DateBoxRatio * 9)
+            Dayborder.backgroundColor = UIColor.red.cgColor
+            $0.layer.addSublayer(Dayborder)
+            Dayborder.isHidden = true
+            
         }
         WeekButtonUI() // 버튼 배치 UI구성
         TimeStackView.then {
@@ -391,6 +427,7 @@ extension CommonRoutineViewController {
     }
     
     func setData() {
+        
         viewModel.dummyObsrvable
             .bind(to: CRcollectionView.rx
                     .items(
@@ -400,36 +437,159 @@ extension CommonRoutineViewController {
                 cell.initUI(of: item)
             }.disposed(by: bag)
         
-//        InnerBoxViewModel.dummyObsrvable
-//            .bind(to: WeekendCollectionView.rx
-//                    .items(
-//                        cellIdentifier: "WeekCell",
-//                        cellType: WeekendCollectionViewCell.self)
-//            ) { index, item, cell in
-//                cell.backgroundColor = .brown
-//                cell.initUI(of: item)
-//            }.disposed(by: bag)
+        YearTextField.rx.text.orEmpty
+            .skip(1) // 구독 시 bind코드가 적용되는데 밑줄이 우리가 포커스를 잡은 시점부터 나타나길 바래서
+            .observe(on: MainScheduler.asyncInstance)
+            .bind{
+                self.YearTextField4($0)
+                self.Yearborder.isHidden = false
+            } // 에러 방출할 일 없으니 bind로 사용해보자
+            .disposed(by: bag) // 메모리 누수를 막읍시다.
         
+        MonthTextField.rx.text.orEmpty
+            .skip(1)
+            .observe(on: MainScheduler.asyncInstance)
+            .subscribe(onNext: {
+                self.MonthTextField2($0)
+                self.Monthborder.isHidden = false
+            })
+            .disposed(by: bag)
+        
+        DayTextField.rx.text.orEmpty
+            .skip(1)
+            .observe(on: MainScheduler.asyncInstance)
+            .bind{
+                self.DayTextField2($0)
+                self.Dayborder.isHidden = false
+            } // 에러 방출할 일 없으니 bind로 사용해보자
+            .disposed(by: bag)
+        
+    }
+    
+    func nowDateTime(_ index : Int) -> String {
+        /*
+         index 0 - year
+         index 1 - month
+         index 2 - day
+         */
+        let today = NSDate() //현재 시각 구하기
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let dateString = dateFormatter.string(from: today as Date)
+        let arr = dateString.components(separatedBy: "-") // 분리해서 내보내주기!
+        print(arr) //"2021"
+        
+        return arr[index]
     }
 }
 
 extension CommonRoutineViewController : UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        
         return CGSize(width: 60, height: 60)
-//        if collectionView == CRcollectionView {
-//            return CGSize(width: 60, height: 60)
-//        } else { // WeekCollectionView
-//            let boxWidth = view.frame.width - (view.frame.width/7)
-//
-//            let collectionSizeWidth = boxWidth - (27 * boxWidth / 374)
-//
-//            let width : CGFloat = collectionSizeWidth / 8
-//            let height : CGFloat = 49 * 150 * DeviceRatio / 219
-//
-//            return CGSize(width: width, height: height) // 이걸 커스텀 레이아웃 사용하면 컨텐츠가 크기로 자동으로 설정할 수 있어서 좋을탠데 ㅜㅜ 커스텀 레이아웃 아직 너무 어려움ㅜㅋ
-//        }
+    }
+}
+
+extension CommonRoutineViewController {   // 텍스트 필드 제약조건을 주기 위해서
+    /*
+     텍스트필드 제약조건에 대해서 고려해야 하는 점들.
+     1. 입력받는 길이를 제한하기 -> 코드로 최대길이 제한
+     2. 입력이 끝나면 자동으로 키보드 내리기 -> 최대길이 도달하면 내리게 코드로 주기
+     3. 터무니없는 입력값을 아예 비워버리기
+     4. 입력이 끝나지 않았다면 다른 곳을 터치하여 포키스를 헤제하기
+     5. 다시 입력할 때, 자동으로 텍스트 필드 비워버리기 -> UX 향상 -> 스토리보드에서 clear when editing begin으로 설정
+     
+     + Month나 Day의 경우에는 한자리만 입력하는 경우가 종종 발생할 수 있는데, 유저의 인식 향상을 위해 placeholder를 제공하면 좋겠다.
+     placeholder에 제공하는 값은 오늘 날짜로 제공하면 좋지 않을까?
+     + Year의 경우에는 대부분 고정되어 있으니 처음부터 값을 줘도 좋겠다!
+     Year을 기본으로 주는 만큼 만약에 현재 날짜 및 시간보다 앞의 값을 입력한다면 입력하지 정보를 정정하고 Year의 조절이 있을 경우 언더라인이 나타나게한다.
+     */
+    private func YearTextField4(_ str : String) { // 최대길이 4까지
+        self.Yearborder.backgroundColor = UIColor.red.cgColor // 입력시 빨간색으로
+        if str.count > 4 {
+            let index = str.index(str.startIndex, offsetBy: 4)
+            self.YearTextField.text = String(str[..<index])
+        } else if str.count == 4 {
+            if Int(str)! < Int(nowDateTime(0))! { // 올해보다 이전이라면
+                self.YearTextField.text = nowDateTime(0) // 올해값으로 정정
+            }
+            
+            if !checkLeapYear() && self.MonthTextField.text == "02" && self.DayTextField.text == "29" { // 윤년이 아닌데, 2월 29일로 설정되어 있다면
+                self.DayTextField.text = "28" // 28일로 변경
+            }
+            
+            self.Yearborder.backgroundColor = UIColor.green.cgColor
+            self.YearTextField.resignFirstResponder() // 키보드 내리기
+        }
+    }
+    private func MonthTextField2(_ str : String) { // 최대길이 2까지
+        self.Monthborder.backgroundColor = UIColor.red.cgColor // 재입력시 빨간색으로
+        if str.count > 2 {
+            let index = str.index(str.startIndex, offsetBy: 2)
+            self.MonthTextField.text = String(str[..<index])
+        } else if str.count == 2 {
+            
+            if Int(str)! > 12 || Int(str)! < 1 { // 여기서 터무니 없는 숫자면 이번달로 설정
+                self.MonthTextField.text = nowDateTime(1)
+            } else { // 정상적으로 입력 되었으면
+                self.Monthborder.backgroundColor = UIColor.green.cgColor
+            }
+            
+            if self.DayTextField.text != "" { // 일이 비어있다면 넘어가도록 설계
+                if checklastDay() < Int(self.DayTextField.text!)! { // 일이 입력되어 있는 상태에서 월을 변경할 때, 마지막 날이 이번달에 없는 날이면 변경하기 위한 로직
+                    self.DayTextField.text = "\(checklastDay())"
+                }
+            }
+            
+            self.MonthTextField.resignFirstResponder() // 키보드 내리기
+        }
+    }
+    private func DayTextField2(_ str : String) { // 최대길이 2까지
+        self.Dayborder.backgroundColor = UIColor.red.cgColor
+        if str.count > 2 {
+            let index = str.index(str.startIndex, offsetBy: 2)
+            self.DayTextField.text = String(str[..<index])
+        } else if str.count == 2 {
+            // 고려 해야할 점 -> 윤년, 월, 년,월이 입력되지 않은 경우
+            if checklastDay() < Int(self.DayTextField.text!)! { // 그 달에 없는 날짜이면
+                self.DayTextField.text = "\(checklastDay())"
+            }
+            self.Dayborder.backgroundColor = UIColor.green.cgColor
+            self.DayTextField.resignFirstResponder() // 키보드 내리기
+        }
+    }
+    
+    func checkLeapYear() -> Bool{ // 윤년인지 아닌지 체크하는 함수
+        
+        guard let year = Int(self.YearTextField.text!) else {
+            self.YearTextField.text = nowDateTime(0)
+            return false
+        }
+        
+        if( (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            return true // 윤년
+        } else {
+            return false // 윤년 아님
+        }
+    }
+    
+    func checklastDay() -> Int { // 이번달이 몇일까지 있는지 체크하는 함수
+        guard let month = Int(self.MonthTextField.text!) else { // 월이 비워져 있으면
+            // 여기서 팁! month가 비었지만, else에서 month가 채워져서 guard문으로 끝나지 않고, switch쪽으로 넘어간다.
+            self.MonthTextField.text = nowDateTime(1) // 월을 채워주고
+            self.Monthborder.backgroundColor = UIColor.green.cgColor
+            return 1
+        }
+        switch month {
+        case 1,3,5,7,8,10,12: // 31일까지 있는 달
+            return 31
+        case 4,6,9,11 : // 30일까지 있는 달
+            return 30
+        case 2 : // 윤년인지 아닌지 판단해서 리턴해줌
+            if checkLeapYear() { return 29 } else { return 28 } // 함수는 3항연산자로 못바꿀까?
+        default:
+            return -1 // 에러나면 -1 리턴해줌
+        }
     }
 }
 
@@ -483,43 +643,3 @@ class CRCollectionViewModel {
     }
 }
 
-struct WeekCollectionModel {
-    var WeekLabel : String
-}
-
-class WeekendCollectionViewCell : UICollectionViewCell {
-    @IBOutlet weak var WeekCellLabel: UILabel!
-    
-    func initUI(of item : WeekCollectionModel) {
-        cellLayout()
-        WeekCellLabel.then {
-            $0.text = item.WeekLabel
-            $0.tintColor = .black
-        }
-    }
-    
-    func cellLayout() {
-        WeekCellLabel.snp.makeConstraints {
-            $0.top.bottom.left.right.equalTo(0)
-        }
-    }
-}
-
-class WeekCollectionViewModel {
-    var dummyData = [
-        WeekCollectionModel(WeekLabel: "매주"),
-        WeekCollectionModel(WeekLabel: "월"),
-        WeekCollectionModel(WeekLabel: "화"),
-        WeekCollectionModel(WeekLabel: "수"),
-        WeekCollectionModel(WeekLabel: "목"),
-        WeekCollectionModel(WeekLabel: "금"),
-        WeekCollectionModel(WeekLabel: "토"),
-        WeekCollectionModel(WeekLabel: "일")
-    ]
-    
-    var dummyObsrvable: Observable<[WeekCollectionModel]>
-    
-    init() {
-        dummyObsrvable = Observable.of(dummyData)
-    }
-}
