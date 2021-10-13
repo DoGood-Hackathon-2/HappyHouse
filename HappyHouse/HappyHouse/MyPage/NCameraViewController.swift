1c7553b8f10900d2c6dfc2f25b872882716c165b
lgvv <lgw9898@naver.com>
2021-10-13 17:56:46 +0900

    챌린지 인증 카메라 모듈 구현 완료

--- HappyHouse/HappyHouse/MyPage/NCameraViewController.swift
+++ HappyHouse/HappyHouse/MyPage/NCameraViewController.swift
@@ -9,14 +9,18 @@ import Foundation
 import UIKit
 import SnapKit
 import Then
+import RxSwift
+import RxCocoa
 
 class NCameraViewController : UIViewController {
     
-    @IBOutlet weak var PhotoImage: UIView!
+    @IBOutlet weak var PhotoImage: UIImageView!
     @IBOutlet weak var PhotoButton: UIButton!
     @IBOutlet weak var MemoTitle: UILabel!
     @IBOutlet weak var MomoView: UITextView!
     @IBOutlet weak var okButton: UIButton!
+    
+    let bag = DisposeBag()
     
     let safetyArea: UIView = {
         let v = UIView()
@@ -26,13 +30,19 @@ class NCameraViewController : UIViewController {
     var DeviceWidth : CGFloat = 0.0
     var DeviceHeight : CGFloat = 0.0
     
+    let picker = UIImagePickerController()
+    
     override func viewDidLoad() {
         DeviceWidth = view.frame.width
         DeviceHeight = view.frame.height
         
+        picker.delegate = self
+        //picker.allowsEditing = true
+        
         setData()
         setBaseView()
         layout()
+        event() 
     }
     
 }
@@ -83,20 +93,95 @@ extension NCameraViewController {
             $0.top.equalTo(MemoTitle.snp.bottom).offset(16)
             $0.left.equalToSuperview().offset(16)
             $0.right.equalToSuperview().offset(-16)
-            $0.bottom.equalTo(okButton).offset(-16)
+            $0.bottom.equalTo(okButton.snp.top).offset(-16)
         }
         okButton.snp.makeConstraints {
             $0.bottom.equalToSuperview().offset(-12)
             $0.left.equalToSuperview().offset(16)
             $0.right.equalToSuperview().offset(-16)
-            $0.height.equalTo(50)
+            $0.height.equalTo(56)
         }
     }
     
     func setData() {
+        
+        _ = PhotoImage.then{
+            $0.backgroundColor = UIColor(red: 0.769, green: 0.769, blue: 0.769, alpha: 1)
+            
+        }
+        
         _ = MemoTitle.then {
-            $0.text = "메모를 입력하세요"
+            $0.text = "간단한 메모도 남길 수 있어요!"
+            $0.textColor = UIColor(red: 0.296, green: 0.296, blue: 0.296, alpha: 1)
+        }
+        
+        _ = MomoView.then {
+            $0.text = "메모를 입력해주세요."
+            $0.textColor = .lightGray
+            $0.layer.cornerRadius = 20
+            $0.backgroundColor = .white
+            $0.layer.shadowColor = UIColor(red: 0.929, green: 0.929, blue: 0.929, alpha: 1).cgColor
+        }
+        
+        _ = okButton.then{
+            $0.setTitle("인증하기", for: .normal)
+            $0.setTitleColor(.white, for: .normal)
+            $0.backgroundColor = UIColor(red: 0.446, green: 0.631, blue: 1, alpha: 1)
+            $0.layer.cornerRadius = (DeviceWidth-32) * 24 / 380
+            
         }
     }
     
+    func event() {
+        MomoView.rx.didBeginEditing
+            .bind{
+                if self.MomoView.text.count == 0 {
+                    self.MomoView.text = "메모를 입력해주세요."
+                    self.MomoView.textColor = UIColor(red: 0.675, green: 0.675, blue: 0.675, alpha: 1)
+                } else {
+                    self.MomoView.text = ""
+                    self.MomoView.textColor = .black
+                    
+                }
+                self.MomoView.textColor = .black
+            }.disposed(by: bag)
+        
+        PhotoButton.rx.tap
+            .bind{
+                let alert = UIAlertController(title: "", message: "갤러리", preferredStyle: .actionSheet)
+                let library = UIAlertAction(title: "사진 앨범", style: .default)  {_ in
+                    self.openLibrary()
+                }
+                let camera = UIAlertAction(title: "카메라", style: .default) {_ in
+                    self.openCamera()
+                }
+                let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+                alert.addAction(library)
+                alert.addAction(camera)
+                alert.addAction(cancel)
+                self.present(alert, animated: true, completion: nil)
+            }
+    }
+}
+
+extension NCameraViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func openLibrary(){
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false, completion: nil)
+    }
+    
+    func openCamera(){
+        if(UIImagePickerController .isSourceTypeAvailable(.camera)) {
+            picker.sourceType = .camera
+            present(picker, animated: false, completion: nil)
+        } else {
+            print("Camera not available")
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            PhotoImage.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
